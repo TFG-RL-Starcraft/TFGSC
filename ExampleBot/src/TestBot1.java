1fab6dc33a9e1a82c39ceac53f875c4df42171c8
Alberto <Alberto@Alberto-PC>
2014-10-16 19:27:38 +0200

    Funcion getWorkers que devuelve el numeros de unidades que son trabajadores

--- ExampleBot/src/TestBot1.java
+++ ExampleBot/src/TestBot1.java
@@ -1,3 +1,7 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 import bwapi.*;
 import bwta.*;
 
@@ -9,6 +13,8 @@ public class TestBot1{
 
     private Player self;
 
+    private int workers; 
+    
     public void run() {
         mirror.getModule().setEventListener(new DefaultBWListener() {
             @Override
@@ -20,7 +26,7 @@ public class TestBot1{
             public void onStart() {
                 game = mirror.getGame();
                 self = game.self();
-
+                workers = getWorkers();
                 //Use BWTA to analyze map
                 //This may take a few minutes if the map is processed first time!
                 System.out.println("Analyzing map...");
@@ -36,18 +42,40 @@ public class TestBot1{
             public void onFrame() {
                 game.setTextSize(10);
                 game.drawTextScreen(10, 10, "Playing as " + self.getName() + " - " + self.getRace());
-
                 StringBuilder units = new StringBuilder("My units:\n");
-
+                
+               
+                
                 //iterate through my units
                 for (Unit myUnit : self.getUnits()) {
                     units.append(myUnit.getType()).append(" ").append(myUnit.getTilePosition()).append("\n");
-
-                    //if there's enough minerals, train an SCV
-                    if (myUnit.getType() == UnitType.Terran_Command_Center && self.minerals() >= 50) {
-                        myUnit.train(UnitType.Terran_SCV);
+                    
+                    if (workers<=8){
+	                    //if there's enough minerals, train an SCV or Drone or Probe
+	                    if (myUnit.getType() == UnitType.Terran_Command_Center && self.minerals() >= 50) {
+	                        myUnit.train(UnitType.Terran_SCV);
+	                        workers++;
+	                    } else if (myUnit.getType() == UnitType.Zerg_Hatchery && self.minerals() >= 50) {
+	                        myUnit.train(UnitType.Zerg_Drone);
+	                        workers++;
+	                    } else if (myUnit.getType() == UnitType.Protoss_Nexus && self.minerals() >= 50) {
+	                        myUnit.train(UnitType.Protoss_Probe);
+	                        workers++;
+	                    }	
                     }
-
+                    
+                    if(workers==9 && myUnit.getType().isWorker()){
+                    	TilePosition t = self.getStartLocation().makeValid();
+                    	if(self.getRace()==Race.Terran)	
+                    		myUnit.build(t, UnitType.Terran_Supply_Depot);
+                    	if(self.getRace()==Race.Zerg)	
+                    		myUnit.build(t, UnitType.Zerg_Overlord);
+                    	if(self.getRace()==Race.Protoss)	
+                    		myUnit.build(t, UnitType.Protoss_Pylon);
+                    		
+                    }
+                    	
+                    
                     //if it's a drone and it's idle, send it to the closest mineral patch
                     if (myUnit.getType().isWorker() && myUnit.isIdle()) {
                         Unit closestMineral = null;
@@ -76,6 +104,17 @@ public class TestBot1{
         mirror.startGame();
     }
 
+    private int getWorkers(){
+    	int numberOfWorkers = 0;
+    	
+    	for (Unit myUnit : self.getUnits()) {
+    		if(myUnit.getType().isWorker())
+    			numberOfWorkers++;
+    	}
+    	
+    	return numberOfWorkers;
+    }
+    
     public static void main(String... args) {
         new TestBot1().run();
     }
